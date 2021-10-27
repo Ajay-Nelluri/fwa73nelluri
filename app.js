ff3078b8b397de1c0efedbd6f84bc369bec5cd08
Ajay-Nelluri <S543773@nwmissouri.edu>
2021-10-27 16:46:31 -0500

    new repo was created with all the javascript files

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var mydataRouter = require('./routes/mydata');
+var bonus = require('./routes/bonus');
+
 
 var app = express();
 
@@ -21,7 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/mydata', mydataRouter);
+app.use('/bonus', bonus)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
